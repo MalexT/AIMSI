779c70efbd07ceefc161b242197502dbc9dd8472
MalexT <aleksa.tedi.markovic@gmail.com>
2021-12-10 09:54:00 +0100

    FIX clipboard!

--- src/clipboardf/clipboard.clj
+++ src/clipboardf/clipboard.clj
@@ -3,5 +3,7 @@
 (defn get-clipboard []
   (.getSystemClipboard (java.awt.Toolkit/getDefaultToolkit)))
 
-(defn spit-clipboard [text]
+(defn spit-clipboard 
+  "Not working properly. This needs to be fixed."
+  [text]
   (.setContents (get-clipboard) (java.awt.datatransfer.StringSelection. text) nil))
